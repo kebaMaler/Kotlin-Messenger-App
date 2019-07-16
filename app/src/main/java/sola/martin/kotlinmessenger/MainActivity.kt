26799169445d5d99b31934c71526d5f836e7c4e7
Martin <sola.martin1@gmail.com>
2019-07-16 22:32:46 +0200

    Register and Login activities are done in layout .xml

--- app/src/main/java/sola/martin/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/sola/martin/kotlinmessenger/MainActivity.kt
@@ -1,12 +1,33 @@
 package sola.martin.kotlinmessenger
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val  TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        register_button_register.setOnClickListener {
+            val email = email_editText_register.text.toString()
+            val password = password_editText_register.text.toString()
+
+            Log.d(TAG,"User e-mail is : $email")
+            Log.d(TAG,"User password is : $password")
+        }
+
+        already_have_account_register.setOnClickListener {
+            Log.d(TAG, "Try to show login activity")
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
