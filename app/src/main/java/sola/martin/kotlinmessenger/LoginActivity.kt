de5e4d3a418a11c181bd2e02449902de47fe0229
Martin <sola.martin1@gmail.com>
2019-07-20 14:07:30 +0200

    Messenger app:  4' 10''

--- app/src/main/java/sola/martin/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/sola/martin/kotlinmessenger/LoginActivity.kt
@@ -2,12 +2,26 @@ package sola.martin.kotlinmessenger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
 
+val  TAG = "LoginActivity"
 class LoginActivity: AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_editText_login.text.toString()
+            val password = password_editText_login.text.toString()
+
+            Log.d(TAG, "Attempt login with email/pw: $email/***")
+        }
+
+        back_to_register_textView_login.setOnClickListener {
+            finish()
+        }
 
     }
 }
--- app/src/main/java/sola/martin/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/sola/martin/kotlinmessenger/MainActivity.kt
@@ -8,24 +8,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val  TAG = "MainActivity"
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
 
         register_button_register.setOnClickListener {
             val email = email_editText_register.text.toString()
             val password = password_editText_register.text.toString()
 
-            Log.d(TAG,"User e-mail is : $email")
-            Log.d(TAG,"User password is : $password")
+            Log.d(TAG, "User e-mail is : $email")
+            Log.d(TAG, "User password is : $password")
+            // Firebase Authentication to create a user with email and password
         }
 
         already_have_account_register.setOnClickListener {
             Log.d(TAG, "Try to show login activity")
 
+            // lunch the login activity somehow
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
